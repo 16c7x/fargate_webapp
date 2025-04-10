34dc40a30d3030a998f191dd9838c034889be34d
Andrew Jones <andrew.jones@puppet.com>
2025-04-10 16:17:58 +0100

    type-o's and renaming

--- webapp_service.tf
+++ webapp_service.tf
@@ -1,14 +1,14 @@
 resource "aws_ecs_service" "webapp" {
   name                 = "webapp-service"
-  cluster              = aws_ecs_cluster.fortress.id
+  cluster              = aws_ecs_cluster.webapp.id
   task_definition      = aws_ecs_task_definition.webapp2.arn
   launch_type          = "FARGATE"
   desired_count        = 1
   force_new_deployment = true
 
   network_configuration {
-    subnets          = aws_subnet.fortress_subnet[*].id
-    security_groups  = [aws_security_group.fortress_sg.id]
+    subnets          = aws_subnet.webapp_subnet[*].id
+    security_groups  = [aws_security_group.webapp_sg.id]
     assign_public_ip = true
   }
   enable_execute_command = true 
